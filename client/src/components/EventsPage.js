aa8e04e920ba99e08dacec0685141cec7d70687c
Sam <siacavazzi@gmail.com>
2023-09-28 16:31:23 -0400

    Update EventsPage.js

--- client/src/components/EventsPage.js
+++ client/src/components/EventsPage.js
@@ -10,14 +10,14 @@ import EventListHeader from "./EventListHeader";
 export default function EventsPage() {
 
     const { events } = useLoaderData()
-    
+
     const [dispEvents, setDispEvents] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
-    const [activeTags, setActiveTags] =  useState([])
+    const [activeTags, setActiveTags] = useState([])
     //console.log(dispEvents[0].tags)
-    
-    useEffect(()=>setDispEvents(events),[])
-    
+
+    useEffect(() => setDispEvents(events), [])
+
 
     const mainContainerStyle = {
         display: 'flex',
@@ -35,7 +35,7 @@ export default function EventsPage() {
                 try {
                     const validJSON = e.tags.replace(/'/g, '"');
                     const tag_arr = JSON.parse(validJSON);
-                    
+
                     // Check if every tag in activeTags is present in tag_arr
                     return activeTags.every(tag => tag_arr.includes(tag));
                 } catch (err) {
@@ -43,15 +43,15 @@ export default function EventsPage() {
                     return false;  // If there's an error (like invalid JSON), exclude the event
                 }
             });
-    
+
             setDispEvents(filtered_events);
         } else {
             setDispEvents(events);  // Reset to all events if no tags are active
         }
     }, [activeTags]);
-    
-    
-    
+
+
+
     const contentContainerStyle = {
         flexGrow: 1,
         marginRight: '170px' // This should be slightly more than the width of the tag container to avoid touching.
@@ -59,32 +59,34 @@ export default function EventsPage() {
 
     // setDisplayEvents(events.filter(event => JSON.stringify(event).toLowerCase().includes(searchTerm.toLowerCase())))
     const event_tags = [
-        "Music", "Happy-hour", "Food", "Networking", "Art", "Workshop", "Sports", 
+        "Music", "Happy-hour", "Food", "Networking", "Art", "Workshop", "Sports",
         "Charity", "Education", "Festival", "Outdoor", "Performance", "Lecture",
         "Seminar", "Conference", "Dance", "Film", "Theater", "Comedy", "Literature",
         "Family-friendly", "Cultural", "Fashion", "Craft", "Exhibition", "Fitness",
-        "Yoga", "Meditation", "Technology", "Fundraiser", "Auction", "Launch", 
-        "Celebration", "Spiritual", "Culinary", "Wine-tasting", "Beer-tasting", 
+        "Yoga", "Meditation", "Technology", "Fundraiser", "Auction", "Launch",
+        "Celebration", "Spiritual", "Culinary", "Wine-tasting", "Beer-tasting",
         "Pop-up", "DIY", "Virtual", "Adventure", "Travel", "Nightlife", "Rave", "Retreat",
         "Reunion", "Gaming", "Role-playing", "Cosplay", "Market", "Trade-show"
-        ]
+    ]
     console.log('Rendering EventsPage with', dispEvents);
     return (
         <>
-        <div>
-        <h1>Events</h1>
-            <EventListHeader />
+            <div>
+                <h1>Events</h1>
+                <EventListHeader />
 
-            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-            {activeTags.map(t => <a onClick={()=>removeTag(t)} class="ui label">{t}</a>)}
+                <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <button class="ui primary button">Show Events Today</button>
+                {activeTags.map(t => <a onClick={() => removeTag(t)} class="ui label">{t}</a>)}
             </div>
-        <div style={mainContainerStyle}>
-            <TagPanel tags={event_tags} activeTags={activeTags} setActiveTags={setActiveTags}/> 
-            <div style={contentContainerStyle}>
-            <EventList events = {dispEvents}/>
-            </div>
+            <div style={mainContainerStyle}>
 
-        </div>
+                <TagPanel tags={event_tags} activeTags={activeTags} setActiveTags={setActiveTags} />
+                <div style={contentContainerStyle}>
+                    <EventList events={dispEvents} />
+                </div>
+
+            </div>
         </>
 
 
